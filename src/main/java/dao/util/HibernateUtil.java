7bebdf63f4e701bb71c96a1b3d2984f18c68f25a
Hasindu Eshan <hasindueshsandesilva@gmail.com>
2023-12-17 15:19:34 +0530

    create HibernateUtil

--- src/main/java/dao/util/HibernateUtil.java
+++ src/main/java/dao/util/HibernateUtil.java
@@ -1,12 +1,31 @@
 package dao.util;
 
+import entity.Customer;
+import entity.Item;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory = createSessionFactory();
     private static SessionFactory createSessionFactory(){
-        return null;
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .addAnnotatedClass(Customer.class)
+                .addAnnotatedClass(Item.class)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        return metadata.getSessionFactoryBuilder()
+                .build();
     }
 
     public static Session getSession(){
--- src/main/java/entity/Item.java
+++ src/main/java/entity/Item.java
@@ -2,12 +2,17 @@ package entity;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @ToString
+@Entity
 public class Item {
+    @Id
     private String code;
     private String description;
     private double unitPrice;
